9b5dd105098d8e65ad219306e3d658d79241368e
Sebastian Vollbrecht <sebivo@freenet.de>
2024-06-07 20:09:34 +0200

    Add `global.d.ts` hint

--- cucumber-feature-import/global.d.ts
+++ cucumber-feature-import/global.d.ts
@@ -1,3 +1,8 @@
+// This file is required to hide the following warning:
+//
+//   Could not find a declaration file for module 'cypress-on-fix', it implicitly has an 'any' type.
+//   Try `npm i --save-dev @types/cypress-on-fix` if it exists or add a new declaration (.d.ts) file
+//   containing `declare module 'cypress-on-fix';`ts(7016)
 declare module "cypress-on-fix" {
   function fix(on: Cypress.PluginEvents): Cypress.PluginEvents;
   export = fix;
